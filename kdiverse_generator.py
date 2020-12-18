21bf17450e4c779ccf23e136569ab75a1b4afaf1
Mukit <mukitrashid270596@gmail.com>
2020-12-18 21:53:36 +0600

    evaluation new 171220

--- kdiverse_generator.py
+++ kdiverse_generator.py
@@ -1,12 +1,56 @@
+import csv
+import os
+
+import kfold_dataset_generator
 import metric
 import numpy as np
 import data_generator
 import graph_embedding_kdiv
 import lstm_model
 import gibbs_sampler
-import time
 
 np.random.seed(1234567890)
+
+def write_to_file(dictionary, directory, isFreq = False):
+
+    if not isFreq:
+        file_path = os.path.join(directory, str(data_generator.dat_suffix[data_generator.dat_ix]) + "_" +
+                                 str(kfold_dataset_generator.mytest_index)) + '.csv'
+    else:
+        file_path = os.path.join(directory, str(data_generator.dat_suffix[data_generator.dat_ix]) + "_" +
+                                 str(kfold_dataset_generator.mytest_index)) + '_freq.csv'
+
+    write_lines = []
+
+    for k, v in dictionary.items():
+        for i in range(len(v)):
+            write_lines.append(v[i])
+        write_lines.append([-1])
+
+    with open(file_path, mode='w+', newline="") as to_csv_file:
+        csv_file_writer = csv.writer(to_csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        for row in write_lines:
+            csv_file_writer.writerow(row)
+
+    return
+
+
+def write_distmat_to_file():
+    file_path = os.path.join('gtset_myalgo',
+                             str(data_generator.dat_suffix[data_generator.dat_ix]) + "_" +
+                             str(kfold_dataset_generator.mytest_index) + "_distmat") + '.csv'
+    total_pois = data_generator.poi_poi_distance_matrix.shape[0]
+    write_lines = [[total_pois]]
+    for i in range(total_pois):
+        write_lines.append(list(data_generator.poi_poi_distance_matrix[i]))
+
+    with open(file_path, mode='w', newline="") as to_csv_file:
+        csv_file_writer = csv.writer(to_csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        for row in write_lines:
+            csv_file_writer.writerow(row)
+
+    return
+
 
 
 def generate_result(load_from_file, K):
@@ -23,6 +67,10 @@ def generate_result(load_from_file, K):
 
     total_traj_curr = 0
     count = 1
+
+    all_gtset = dict()
+    all_gtfreqset = dict()
+    all_recset = dict()
 
     # st = time.time()
     for k, v in data_generator.test_data_dicts_vi[0].items():
@@ -64,6 +112,11 @@ def generate_result(load_from_file, K):
         count += 1
         print([poi_start, poi_end])
         print(all_traj)
+
+        all_gtset[k] = v
+        all_gtfreqset[k] = [data_generator.test_data_dicts_vi[2][k]]
+        all_recset[k] = all_traj
+
         total_score_curr_f1 += metric.tot_f1_evaluation(v, data_generator.test_data_dicts_vi[2][k], all_traj)
         total_score_curr_pf1 += metric.tot_pf1_evaluation(v, data_generator.test_data_dicts_vi[2][k], all_traj)
         total_traj_curr += np.sum(data_generator.test_data_dicts_vi[2][k]) * len(all_traj)
@@ -86,6 +139,11 @@ def generate_result(load_from_file, K):
     avg_intra_f1 = np.average(np.array(all_intra_f1_scores))
 
     print("F1: " + str(avg_f1) + " PF1: " + str(avg_pf1) + " IOU: " + str(avg_iou) + " IF1: " + str(avg_intra_f1))
+
+    write_to_file(all_gtset, 'gtset_myalgo')
+    write_to_file(all_gtfreqset, 'gtset_myalgo', isFreq=True)
+    write_to_file(all_recset, 'recset_myalgo')
+    write_distmat_to_file()
 
     # en = time.time()
     # print(en-st)
